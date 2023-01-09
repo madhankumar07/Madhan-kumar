58b597273eb48b537aaa78b5dc663398ec2de187
madhankumar07 <122252714+madhankumar07@users.noreply.github.com>
2023-01-09 14:06:36 +0530

    Add files via upload

---
+++ easy5.sqcu.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int sq,cu,num;
+	printf("enter the integer");
+	scanf("%d",&num);
+	sq=num*num;
+	cu=num*num*num;
+	printf("square number  %d\n",sq);
+	printf("cube number %d\n",cu);
+	return 0;
+}
